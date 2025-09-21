6ad70441c55a5040a79e783ba9fb02f381c18535
Amy Ngo <141047667+amydn16@users.noreply.github.com>
2025-09-21 14:48:23 -0400

    Fix FileNotFoundError in 02_data_ingestion.py (#4)

--- course/notebooks/02_week/02_data_ingestion.py
+++ course/notebooks/02_week/02_data_ingestion.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from pathlib import Path
 
-df = pd.read_csv('course/datasets/test.csv')
+course_dir = Path.cwd().parent.parent
+data_path = course_dir / "datasets"
+
+test_data_path = data_path / "test.csv"
+df = pd.read_csv(test_data_path.as_posix())
 print(df.head())
