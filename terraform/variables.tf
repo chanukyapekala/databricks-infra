54b1a80612c481a15f7833ab07c250f5dbb65b79
Chanukya Pekala <chanukya.pekala@gmail.com>
2025-09-28 16:27:18 +0300

    Adding 0x week

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,13 +6,17 @@ variable "notebook_language" {
 
 variable "notebook_subdirs" {
   type    = list(string)
-  default = ["01_week", "02_week"]
+  default = ["0x_week", "01_week", "02_week"]
 }
 
 variable "notebooks" {
   type = map(string)
   default = {
-    "01_week/01_getting_started.py" = "PYTHON"
-    "02_week/02_data_ingestion.py"  = "PYTHON"
+    "0x_week/00_databricks_fundamentals.py" = "PYTHON"
+    "0x_week/01_unity_catalog_deep_dive.py" = "PYTHON"
+    "0x_week/02_cluster_management.py"      = "PYTHON"
+    "0x_week/03_spark_on_databricks.py"     = "PYTHON"
+    "01_week/01_getting_started.py"        = "PYTHON"
+    "02_week/02_data_ingestion.py"         = "PYTHON"
   }
 }
