829fff14aa59e5c288eef228675e87caa24f1d88
Chanukya Pekala <chanukya.pekala@gmail.com>
2025-09-28 22:03:12 +0300

    Weeks' structure reorganized

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,17 +6,36 @@ variable "notebook_language" {
 
 variable "notebook_subdirs" {
   type    = list(string)
-  default = ["0x_week", "01_week", "02_week"]
+  default = ["01_week", "02_week", "03_week", "04_week", "05_week"]
 }
 
 variable "notebooks" {
   type = map(string)
   default = {
-    "0x_week/00_databricks_fundamentals.py" = "PYTHON"
-    "0x_week/01_unity_catalog_deep_dive.py" = "PYTHON"
-    "0x_week/02_cluster_management.py"      = "PYTHON"
-    "0x_week/03_spark_on_databricks.py"     = "PYTHON"
-    "01_week/01_getting_started.py"        = "PYTHON"
-    "02_week/02_data_ingestion.py"         = "PYTHON"
+    # Week 1: Databricks Fundamentals
+    "01_week/00_databricks_fundamentals.py" = "PYTHON"
+    "01_week/01_unity_catalog_deep_dive.py" = "PYTHON"
+    "01_week/02_cluster_management.py"      = "PYTHON"
+    "01_week/03_spark_on_databricks.py"     = "PYTHON"
+    
+    # Week 2: Data Ingestion
+    "02_week/04_file_ingestion.py"          = "PYTHON"
+    "02_week/05_api_ingest.py"              = "PYTHON"
+    "02_week/06_database_ingest.py"         = "PYTHON"
+    "02_week/07_s3_ingest.py"               = "PYTHON"
+    
+    # Week 3: Data Transformations
+    "03_week/08_simple_transformations.py"  = "PYTHON"
+    "03_week/09_window_transformations.py"  = "PYTHON"
+    "03_week/10_aggregations.py"            = "PYTHON"
+    
+    # Week 4: End-to-End Workflows
+    "04_week/11_file_to_aggregation.py"     = "PYTHON"
+    "04_week/12_api_to_aggregation.py"      = "PYTHON"
+    
+    # Week 5: Job Orchestration
+    "05_week/13_create_job_with_notebook.py" = "PYTHON"
+    "05_week/14_create_job_with_wheel.py"    = "PYTHON"
+    "05_week/15_orchestrate_tasks_in_job.py" = "PYTHON"
   }
 }
